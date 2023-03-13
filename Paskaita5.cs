fc0cf6340f88f893fd8d723a808e688167d4f398
Lunaleki <109433846+Lunaleki@users.noreply.github.com>
2023-03-13 19:15:08 +0200

    Add files via upload

---
+++ Paskaita5.cs
@@ -0,0 +1,44 @@
+//Console.WriteLine("labas");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaskaitosProjektas
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //"5" - zodis
+            //5 - int, sveikas skaicius
+            //5.0 - double, su kableliu
+            //Zodziai ir Skaiciai
+
+             Console.WriteLine("-------------------------------");
+             Console.WriteLine("|                             |");
+             Console.WriteLine("|                             |");
+             Console.WriteLine("|         Mano vardas         |");
+             Console.WriteLine("|            Renata           |");
+             Console.WriteLine("|                             |");
+             Console.WriteLine("|                             |");
+             Console.WriteLine("-------------------------------");
+
+        
+           
+             
+
+
+
+            
+
+
+        }
+    }
+}
+
+   
+    
